0931ba694f1707d8714ad4c8a3bfeb15c2dfc1a0
Gijs van der Voort <gijs@Gijs-Laptop-Mint.(none)>
2011-11-18 11:16:27 +0100

    Kleine tweaks

--- src/LetterCropper.py
+++ src/LetterCropper.py
@@ -1,4 +1,3 @@
-from PIL import Image
 from pylab import *
 
 class LetterCropper:
--- src/LetterCropperTest.py
+++ src/LetterCropperTest.py
@@ -1,5 +1,5 @@
 from pylab import *
-from crop import LetterCropper
+from LetterCropper import LetterCropper
 
 letter_cropper = LetterCropper("../images/test.png")
 cropped_letter = letter_cropper.get_cropped_letter()
