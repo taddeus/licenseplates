c6ab9323bae324c84d851236baf5796126ca63f7
Jayke Meijer <jayke.meijer@gmail.com>
2011-12-01 18:10:45 +0100

    Modified Point init to accept a tuple of coordinates as well, for easy testing. Should be backwards compatible.

--- src/Point.py
+++ src/Point.py
@@ -1,4 +1,8 @@
 class Point:
-    def __init__(self, corner):
-        self.x = corner.getAttribute("x")
-        self.y = corner.getAttribute("y")
+    def __init__(self, corner = None, coordinates = None):
+        if corner != None:
+            self.x = corner.getAttribute("x")
+            self.y = corner.getAttribute("y")
+        elif coordinates != None:
+            self.x = coordinates[0]
+            self.y = coordinates[1]
