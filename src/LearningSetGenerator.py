d3e13b6f0b666fe95af8e941a1cacee1a957b08b
Fabien <fabientesselaar@gmail.com>
2011-12-14 17:59:35 +0100

    ugh rare printjes weggehaald

--- src/LearningSetGenerator.py
+++ src/LearningSetGenerator.py
@@ -1,10 +1,10 @@
 from xml_helper_functions import xml_to_LicensePlate
 
-for i in range(1):
-    for j in range(1):
-
+for i in range(9):
+    for j in range(100):
+        try:
             filename = '%04d/00991_%04d%02d' % (i, i, j)
             print 'loading file "%s"' % filename
-
             plate = xml_to_LicensePlate(filename, save_character=1)
-            print plate.characters[0].value;
+        except:
+            print 'epic fail'
