f8f630f0b0da6088ff2d07d93925b8d0e3044195
Taddeus Kroes <taddeuskroes@hotmail.com>
2011-12-20 23:48:53 +0100

    Removed use of get_bin_index function for efficiency reasons.

--- src/Histogram.py
+++ src/Histogram.py
@@ -6,12 +6,14 @@ class Histogram:
         self.max = max
 
     def add(self, number):
-        bin_index = self.get_bin_index(number)
-        self.bins[bin_index] += 1
+        #bin_index = self.get_bin_index(number)
+        #self.bins[bin_index] += 1
+        self.bins[number] += 1
 
     def remove(self, number):
-        bin_index = self.get_bin_index(number)
-        self.bins[bin_index] -= 1
+        #bin_index = self.get_bin_index(number)
+        #self.bins[bin_index] -= 1
+        self.bins[number] -= 1
 
     def get_bin_index(self, number):
         return (number - self.min) / ((self.max - self.min) / len(self.bins))
