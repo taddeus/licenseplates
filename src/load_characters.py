1a203e674e17cf7ce11b27349106a03f3b7da627
Taddeus Kroes <taddeuskroes@hotmail.com>
2011-12-20 19:31:23 +0100

    Merged test scripts into a single test file: find_svm_parameter.py.

--- src/load_characters.py
+++ src/load_characters.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 from os import listdir
 from cPickle import dump
-from pylab import imshow, show
+from sys import argv, exit
 
 from GrayscaleImage import GrayscaleImage
 from NormalizedCharacterImage import NormalizedCharacterImage
 from Character import Character
+
+if len(argv) < 4:
+    print 'Usage: python %s FILE_SUFFIX BLUR_SCALE NEIGHBOURS' % argv[0]
+    exit(1)
 
 c = []
 
@@ -13,12 +17,13 @@ for char in sorted(listdir('../images/LearningSet')):
     for image in sorted(listdir('../images/LearningSet/' + char)):
         f = '../images/LearningSet/' + char + '/' + image
         image = GrayscaleImage(f)
-        norm = NormalizedCharacterImage(image, blur=1, size=(48, 36))
-        #imshow(norm.data, cmap='gray')
-        #show()
+        norm = NormalizedCharacterImage(image, blur=float(argv[2]), height=42)
+        #from pylab import imshow, show
+        #imshow(norm.data, cmap='gray'); show()
         character = Character(char, [], norm)
-        character.get_single_cell_feature_vector()
+        character.get_single_cell_feature_vector(int(argv[3]))
         c.append(character)
         print char
 
-dump(c, open('characters.dat', 'w+'))
+print 'Saving characters...'
+dump(c, open('characters%s.dat' % argv[1], 'w+'))
